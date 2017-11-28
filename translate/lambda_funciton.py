7e6a242529f65ce906bfe8c46d6aede52c8ac4a7
Joe McGinty <joe.mcginty@hivehome.com>
2017-11-27 20:55:40 -0800

    Lambda updates

--- translate/lambda_funciton.py
+++
@@ -1,9 +0,0 @@
-from googletrans import Translator
-translator = Translator()
-
-def lambda_handler(event, context):
-    # TODO implement
-    translation = translator.translate("Hello from Lambda", dest="ko")
-
-    return translation.text
-
--- twilio/lambda_function.py
+++ twilio/lambda_function.py
@@ -4,19 +4,22 @@ import StringIO
 import urllib2
 import uuid
 import json
+import time
 
 from boto3.dynamodb.conditions import Key
 from boto3.session import Session
 from PIL import Image, ImageOps, ImageDraw
 from twilio.rest import Client
+from googletrans import Translator
 
 # create Twilio session
 # Add Twilio Keys
-account_sid = ""
-auth_token = ""
+account_sid = "AC6e7b4be234c3fdf028d93dbc23765468"
+auth_token = "dbd7e8fe77622e5eb41a30abe5eaf033"
 phone_number = "+17255021401"
 sns_completion_topic = "arn:aws:sns:us-west-1:878174247419:charity_sent_message"
 client = Client(account_sid, auth_token)
+translator = Translator()
 
 # create an S3 & Dynamo session
 s3 = boto3.resource('s3')
@@ -32,10 +35,10 @@ def get_user_details(from_number):
     response_dynamo = table_users.query(KeyConditionExpression=Key('fromNumber').eq(from_number))
     if response_dynamo['Count'] != 1:
         return None
-    return (response_dynamo['Items'][0]['name'], response_dynamo['Items'][0]['charity'])
+    return (response_dynamo['Items'][0]['name'], response_dynamo['Items'][0]['language'], response_dynamo['Items'][0]['charity'])
 
-def save_message(message_id, user, charity, message, media_url):
-    table_messages.put_item(Item={'message_id': message_id, 'user_id': user, 'charity_id': charity, 'message': message, 'media_url': media_url})
+def save_message(message_id, user, charity, message, media_url, timestamp):
+    table_messages.put_item(Item={'message_id': message_id, 'user_id': user, 'charity_id': charity, 'message': message, 'media_url': media_url, 'timestamp':timestamp})
 
 
 def handle_media(charity, message_id, pic_url):
@@ -59,10 +62,14 @@ def handle_media(charity, message_id, pic_url):
     output.close()
 
     s3.Bucket(bucket).put_object(Key=key, Body=im_data, ACL='public-read', ContentType='image/png', Metadata=m_data)
-    return media_url
+    item_url = "https://{0}.s3.amazonaws.com/{1}".format(bucket, str(key))
+    return item_url
 
-def fire_sns(charity, message):
-    message = {'charity_id': charity, 'message': message}
+def translate_message(message, lang):
+   return translator.translate(message, src=lang, dest="en").text
+
+def fire_sns(charity, media_url, message):
+    message = {'charity_id': charity, 'message': message, 'media_url': media_url}
     client = boto3.client('sns')
     response = client.publish(
         TargetArn=sns_completion_topic,
@@ -78,9 +85,11 @@ def lambda_handler(event, context):
     num_media = event['numMedia']
 
     # check if we have their number
-    user, charity = get_user_details(from_number)
+    user, language, charity = get_user_details(from_number)
     if user == None:
         return "Cannot uniquely identify user"
+
+    message = translate_message(message, language)
 
     message_id = str(uuid.uuid4());
 
@@ -88,8 +97,9 @@ def lambda_handler(event, context):
     if num_media != '0':
         media_url = handle_media(charity, message_id, pic_url)
 
-    save_message(message_id, user, charity, message, media_url)
-    fire_sns(charity, message)
+    timestamp = int(time.time())
+    save_message(message_id, user, charity, message, media_url, timestamp)
+    fire_sns(charity, media_url, message)
 
     return "Thank you for your update"
 
@@ -101,4 +111,5 @@ def sample_filter(im):
     white= "#99CCFF"
     filter_image = ImageOps.colorize(ImageOps.grayscale(im), black, white)
     return filter_image
+
 
